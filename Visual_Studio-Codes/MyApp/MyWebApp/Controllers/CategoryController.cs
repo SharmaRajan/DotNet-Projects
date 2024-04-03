880691c2f3d7125574e1abc0aa112ead0610a144
SharmaRajan <r7870062870@gmail.com>
2024-04-03 10:01:57 +0530

    Added css file and MyWebApp Project

---
+++ Visual_Studio-Codes/MyApp/MyWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Data;
+using MyWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MyWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private AppDBContext _context;
+
+        public CategoryController(AppDBContext context)
+        {
+            _context = context;
+        }
+
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            IEnumerable<CategoryModel> categories = _context.Categories;
+            return View(categories);
+        }
+    }
+}
+
---
+++ Visual_Studio-Codes/MyApp/MyWebApp/Data/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using MyWebApp.Models;
+
+namespace MyWebApp.Data
+{
+	public class AppDBContext : DbContext
+	{
+
+		public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<CategoryModel> Categories { get; set; }
+
+	}
+}
+
